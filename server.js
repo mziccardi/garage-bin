49e60c3ad5ffa7e2c6e2ab39b998f946785cb0ae
Mike Ziccardi <mikeziccardi@gmail.com>
2017-04-13 09:54:13 -0600

    html is rendering, app.locals is working on get items

--- server.js
+++ server.js
@@ -10,8 +10,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Garage-Bin'
+app.use(express.static('public'))
 
+app.locals.title = 'Garage-Bin'
+app.locals.items = [{
+  id:1,
+  name: 'car',
+  reason: 'parked',
+  cleanliness:'sparkling'
+}]
+
+app.get('/', (request, response)=>{
+  fs.readFile(`${__dirname}/index.html`,(err,file)=>{
+    response.send(file)
+  })
+})
+
+app.get('/api/items',(request,response)=>{
+  const items = app.locals.items
+  response.json(items)
+})
 
 
 app.listen(app.get('port'), ()=>{
