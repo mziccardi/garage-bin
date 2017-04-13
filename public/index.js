836c85fc35a58f8db4eb69d0a91c1c6a296c9fcf
Mike Ziccardi <mikeziccardi@gmail.com>
2017-04-13 11:09:14 -0600

    post working form jquery

--- public/index.js
+++ public/index.js
@@ -2,14 +2,32 @@ const name = $('#name')
 const reason = $('#reason')
 const cleanliness = $('#clean')
 
-const createItem = ()=>{
-  axios.post('/api/items', {
-
-  })
-}
 
 $('.submit').on('click',(e)=>{
   e.preventDefault()
-  let cleanstuff = $('#clean').find(":selected").val()
-  console.log(cleanstuff);
+  const newItem = {
+    name:name.val(),
+    reason:reason.val(),
+    cleanliness:cleanliness.find(":selected").val()
+  }
+  createItem(newItem)
 })
+
+
+
+const createItem = (newItem)=>{
+  axios.post('/api/items', newItem)
+  .then((response)=>{
+    console.log(response.data);
+  })
+}
+
+
+
+
+
+// $('.submit').on('click',(e)=>{
+//   e.preventDefault()
+//   let cleanstuff = $('#clean').find(":selected").val()
+//   console.log(cleanstuff);
+// })
