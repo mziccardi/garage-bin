236838dde4134c224f893d3001e16007ee55c190
Mike Ziccardi <mikeziccardi@gmail.com>
2017-04-13 13:46:36 -0600

    all counters working

--- public/index.js
+++ public/index.js
@@ -56,6 +56,9 @@ const countItems = ()=>{
   .then((response)=>{
     $('.item-count').html(`Total number of items: ${response.data.length}`)
   })
+  countSparklingItems()
+  countDustyItems()
+  countRancidItems()
 }
 
 const countSparklingItems = ()=>{
@@ -68,11 +71,49 @@ const countSparklingItems = ()=>{
   })
 
 }
+const countDustyItems = ()=>{
+  axios.get('/api/items')
+  .then((response)=>{
+    let allItems= response.data
+    console.log(allItems);
+    let counter = countDustyHelper(allItems)
+    $('.dusty-count').html(`Number of dusty items: ${counter}`)
+  })
+
+}
+const countRancidItems = ()=>{
+  axios.get('/api/items')
+  .then((response)=>{
+    let allItems= response.data
+    console.log(allItems);
+    let counter = countRancidHelper(allItems)
+    $('.rancid-count').html(`Number of Rancid items: ${counter}`)
+  })
+
+}
 
 const countSparkleHelper = (allItems)=>{
   let count = 0
   for(let i=0; i<allItems.length; i++){
     if(allItems[i].cleanliness === 'sparkling'){
+      count++
+    }
+  }
+  return count
+}
+const countDustyHelper = (allItems)=>{
+  let count = 0
+  for(let i=0; i<allItems.length; i++){
+    if(allItems[i].cleanliness === 'dusty'){
+      count++
+    }
+  }
+  return count
+}
+const countRancidHelper = (allItems)=>{
+  let count = 0
+  for(let i=0; i<allItems.length; i++){
+    if(allItems[i].cleanliness === 'rancid'){
       count++
     }
   }
